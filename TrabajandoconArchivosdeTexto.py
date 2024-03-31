23d5c789dd519c2a98e57958f6448e534d998845
Byron Alvarado <be.alvaradog@uea.edu.ec>
2024-03-31 10:36:50 -0500

    Tarea Semana 16

--- TrabajandoconArchivosdeTexto.py
+++ TrabajandoconArchivosdeTexto.py
@@ -1,26 +1,26 @@
 # Creamos un nuevo archivo llamado my_notes.txt.
 my_notes = open('my_notes.txt', 'w')
 
-# El Método write(): escribir una línea a la vez
-my_notes.write("Línea 1: Hola, soy Byron Alvarado.\n")
-my_notes.write("Línea 2: Hicimos el deber en python.\n")
+# El método write(): escribir una línea a la vez
+my_notes.write("Línea 1: Hola, Soy Byron Alvarado.\n")
+my_notes.write("Línea 2: Prueba método write.\n")
 
-# El Método writelines(): escribir una lista de líneas
-lineas = ["Línea 3: Probamos otro método.\n", "Línea 4: Terminamos el deber.\n"]
+# El método writelines(): escribir una lista de líneas
+lineas = ["Línea 3: Prueba método writelines.\n", "Línea 4: Hasta Pronto.\n"]
 my_notes.writelines(lineas)
 
 my_notes.close()
 
-# Hacemos el Método 1. read()
-# Abrimos el archivo my_notes.txt.
+# Probamos el método 1. read()
+# Abrimos el archivo my_notes.txt y lo ponemos para leer.
 my_notes = open('my_notes.txt', 'r')
-#Leemos el contenido del archivo línea por línea utilizando el método adecuado.
+#Lee el contenido del archivo línea por línea utilizando el método adecuado.
 print('Método 1: read()')
 print('--------------------')
 print(my_notes.read())
 my_notes.close()
 
-# Hacemos el Método 2. readlines()
+# Método 2. readlines()
 my_notes = open('my_notes.txt', 'r')
 print('Método 2: readlines()')
 print('--------------------')
